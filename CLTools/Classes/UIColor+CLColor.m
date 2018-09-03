bc8e068242a3879a2e1936d9b3cce2888dedb935
claudeli <claudeli@yeah.net>
2018-09-03 10:56:08 +0800

    rgba

--- CLTools/Classes/UIColor+CLColor.m
+++ CLTools/Classes/UIColor+CLColor.m
@@ -10,20 +10,20 @@
 
 @implementation UIColor (CLColor)
 
+UIColor *rgba(CGFloat r, CGFloat g, CGFloat b, CGFloat a){
+    return [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
+}
+
+UIColor *rgbaDP3(CGFloat r, CGFloat g, CGFloat b, CGFloat a) {
+    return [UIColor colorWithDisplayP3Red:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
+}
+
 // 随机色
 +(UIColor *)randomColor{
     CGFloat hue = ( arc4random() % 256 / 256.0 ); //0.0 to 1.0
     CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5; // 0.5 to 1.0,away from white
     CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5; //0.5 to 1.0,away from black
     return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-}
-
-+(UIColor *)colorWithR:(CGFloat)r g:(CGFloat)g b:(CGFloat)b a:(CGFloat)a{
-    return [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
-}
-
-+(UIColor *)colorWithDP3R:(CGFloat)r g:(CGFloat)g b:(CGFloat)b a:(CGFloat)a{
-    return [UIColor colorWithDisplayP3Red:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
 }
 
 @end
