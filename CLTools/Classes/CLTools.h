b5c502792dd08f3d8f9c0166312b3de274575c25
claudeli <claudeli@yeah.net>
2018-09-19 18:23:24 +0800

    v1.0.6

--- CLTools/Classes/CLTools.h
+++ CLTools/Classes/CLTools.h
@@ -14,22 +14,24 @@ FOUNDATION_EXPORT const unsigned char CLToolsVersionString[];
 
 #import <CLTools/NSDictionary+CLDictionary.h>
 #import <CLTools/NSString+CLString.h>
+#import <CLTools/NSObject+CLFunc.h>
+
 #import <CLTools/UIColor+CLColor.h>
 #import <CLTools/UIView+CLView.h>
 #import <CLTools/UIDevice+CLDevice.h>
 #import <CLTools/UIImage+CLImage.h>
 #import <CLTools/UIButton+CLButton.h>
-#import <CLTools/CLFunctions.h>
 
 #else
 
 #import "NSDictionary+CLDictionary.h"
 #import "NSString+CLString.h"
+#import "NSObject+CLFunc.h"
+
 #import "UIColor+CLColor.h"
 #import "UIView+CLView.h"
 #import "UIDevice+CLDevice.h"
 #import "UIImage+CLImage.h"
 #import "UIButton+CLButton.h"
-#import "CLFunctions.h"
 
 #endif /* CLTools_h */
